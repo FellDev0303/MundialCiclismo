a0a8f6fd154b601d77fe5ce9d5d2e42f0ed00563
FellDev0303 <josejose2868@hotmail.com>
2025-10-17 12:20:47 -0500

    Se crea la clase persona para fines de herencia

---
+++ src/main/java/Modelo/Persona.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Persona {
+
+    private String nombre;
+    private int edad;
+    private String pais;
+
+    public Persona(String nombre, int edad, String pais) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.pais = pais;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+
+}
