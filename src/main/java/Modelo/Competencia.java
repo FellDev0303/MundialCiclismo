55647b959c4a99f021c60e40c5c768e984669838
veronz07 <veronica.nunez@uao.edu.co>
2025-10-17 12:52:26 -0500

    Se crea la clase Competencia

---
+++ src/main/java/Modelo/Competencia.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Competencia {
+    private String nombreCompetencia;
+    private Equipo[] equipos;
+    private int contadorEquipos;
+
+    public Competencia(String nombreCompetencia) {
+        this.nombreCompetencia = nombreCompetencia;
+        this.equipos = new Equipo[30];
+        this.contadorEquipos = 0;
+    }
+
+    public boolean agregarEquipo(Equipo e) {
+        if (contadorEquipos < equipos.length) {
+            equipos[contadorEquipos++] = e;
+            return true;
+        }
+        return false;
+    }
+
+    public int getContadorEquipos() {
+        return contadorEquipos;
+    }
+
+    public Equipo getEquipo(int indice) {
+        if (indice >= 0 && indice < contadorEquipos) {
+            return equipos[indice];
+        }
+        return null;
+    }
+
+}
