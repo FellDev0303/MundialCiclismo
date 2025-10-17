7d6ec9eae67a66ba24988c5aed1c41cd04eb9174
FellDev0303 <josejose2868@hotmail.com>
2025-10-17 12:50:37 -0500

    Se agrega a la rama "master" la clase competidor.java para fines de completitud

---
+++ src/main/java/Modelo/Competidor.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Competidor extends Persona {
+
+    private int rankingMundial;
+    private double estatura;
+    private double peso;
+    private int puntosTotales;
+
+    public Competidor(String nombre, int edad, String pais, int rankingMundial, double estatura, double peso) {
+        super(nombre, edad, pais);
+        this.rankingMundial = rankingMundial;
+        this.estatura = estatura;
+        this.peso = peso;
+        this.puntosTotales = 0;
+    }
+
+    // Métodos getter y setter
+    public int getRankingMundial() {
+        return rankingMundial;
+    }
+
+    public double getEstatura() {
+        return estatura;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public int getPuntosTotales() {
+        return puntosTotales;
+    }
+
+    public void setRankingMundial(int rankingMundial) {
+        this.rankingMundial = rankingMundial;
+    }
+    public void actualizarRanking(int puntosGanados) {
+        puntosTotales += puntosGanados;
+        rankingMundial -= puntosGanados / 10;
+        if (rankingMundial < 1) {
+            rankingMundial = 1;
+        }
+    }
+
+    public void actualizarRanking(double puntosGanados) {
+        puntosTotales += (int) puntosGanados;
+        rankingMundial -= (int) (puntosGanados / 5);
+        if (rankingMundial < 1) {
+            rankingMundial = 1;
+        }
+    }
+
+    @Override
+    public String obtenerDatos() {
+        return super.obtenerDatos()
+                + ", Ranking: " + rankingMundial
+                + ", Puntos: " + puntosTotales
+                + ", Estatura: " + estatura + "m, Peso: " + peso + "kg";
+    }
+
+    @Override
+    public String toString() {
+        return "Competidor -> " + obtenerDatos();
+    }
+}
