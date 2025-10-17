169ac9aa8a86d7cced9a011693e731521a41bb08
FellDev0303 <josejose2868@hotmail.com>
2025-10-17 12:46:07 -0500

    Se crea la clase Equipo.java para organizar a los competidores por equipo

---
+++ src/main/java/Modelo/Equipo.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Equipo {
+
+    private String nombre;
+    private String pais;
+    private Competidor[] competidores;
+    private int contadorCompetidores;
+
+    public Equipo(String nombre, String pais) {
+        this.nombre = nombre;
+        this.pais = pais;
+        this.competidores = new Competidor[10];
+        this.contadorCompetidores = 0;
+    }
+
+    public boolean agregarCompetidor(Competidor c) {
+        if (contadorCompetidores < competidores.length) {
+            competidores[contadorCompetidores++] = c;
+            return true;
+        }
+        return false;
+    }
+
+    public Competidor getCompetidor(int indice) {
+        if (indice >= 0 && indice < contadorCompetidores) {
+            return competidores[indice];
+        }
+        return null;
+    }
+
+    public int getContadorCompetidores() {
+        return contadorCompetidores;
+    }
+
+
+}
