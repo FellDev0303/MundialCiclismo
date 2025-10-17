ee4d3958812141211e28a0c58dbff62e3bd872c5
FellDev0303 <josejose2868@hotmail.com>
2025-10-17 13:01:09 -0500

    Se crea el método registrarEquipo

--- src/main/java/Controlador/ControladorCompetencia.java
+++ src/main/java/Controlador/ControladorCompetencia.java
@@ -15,5 +15,19 @@ public class ControladorCompetencia {
 
     private Competencia competencia;
 
-   
+    public ControladorCompetencia(String nombreEvento) {
+        this.competencia = new Competencia(nombreEvento);
+    }
+
+    public void registrarEquipo() {
+        String nombre = JOptionPane.showInputDialog("Nombre del equipo:");
+        String pais = JOptionPane.showInputDialog("País del equipo:");
+        Equipo eq = new Equipo(nombre, pais);
+        if (competencia.agregarEquipo(eq)) {
+            JOptionPane.showMessageDialog(null, "Equipo registrado exitosamente.");
+        } else {
+            JOptionPane.showMessageDialog(null, "No se pudo registrar el equipo (límite alcanzado).");
+        }
+    }
+
 }
